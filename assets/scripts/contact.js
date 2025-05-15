588ce69f9cd21603f9f271674d9c3325c33a4baf
Tim Kung <timothytzkung@gmail.com>
2025-05-15 16:46:30 -0700

    Update contact.js
    - updated email

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -8,7 +8,7 @@ document.querySelector('.contact-form').addEventListener('submit', function(e) {
     const subject = document.querySelector('input[name="subject"]').value;
     const message = document.querySelector('textarea[name="message"]').value;
 
-    const recipient = "timothytzkung@gmail.com";
+    const recipient = "tim_kung@sfu.ca";
 
     const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
         `Name: ${name}\nContact Email: ${email}\n\n${message}`
